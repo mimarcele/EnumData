785cda005eee01662fa4b8e1083ef1a7a50c6bbf
Milena Marcele <milenamarcele777@gmail.com>
2020-09-16 10:23:20 -0300

    Testando "values()"

---
+++ src/Enum/TesteValues.java
@@ -0,0 +1,17 @@
+package Enum;
+
+public class TesteValues {
+    public static void main(String[] args) {
+
+        DiaSemana [] dias = DiaSemana.values ();
+
+   /* for(int i = 0; i<dias.length; i++){
+        System.out.println (dias[i]);*/
+        for (DiaSemana dia: DiaSemana.values ()){
+            System.out.println (dia );
+        }
+
+    }
+
+
+}
